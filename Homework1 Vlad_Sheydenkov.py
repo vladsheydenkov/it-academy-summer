5f218658d1f23270663501d6109b7525486072ff
vladsheydenkov <vladsheydenkov@gmail.com>
2020-05-08 23:55:54 +0300

    Homework1 : Vlad Sheydenkov

---
+++ Homework1 Vlad_Sheydenkov.py
@@ -0,0 +1,48 @@
+#1 line: Output
+print ('Start programm #1')
+print('Hello, world')
+
+#2 lines: Input, assignment
+print ('Start programm #2')
+name  = input('What is your name?\n')
+print('Hi, %s.' % name)
+
+#3 lines: For loop, built-in enumerate function, new style formatting
+print ('Start programm #3')
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
+
+#4 lines: Fibonacci, tuple assignment
+print ('Start programm #4')
+parents, babies = (1, 1)
+while babies < 100:
+    print ('This generation has {0} babies'.format(babies))
+    parents, babies = (babies, parents + babies)
+
+#5 lines: Functions
+print ('Start programm #5')
+def greet(name):
+    print ('Hello', name)
+greet('Jack')
+greet('Jill')
+greet('Bob')
+
+#6 lines: Import, regular expressions
+print ('Start programm #6')
+import re
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print (test_string, 'is a valid US local phone number')
+    else:
+        print (test_string, 'rejected')
+
+#7 lines: Dictionaries, generator expressions
+print ('Start programm #7')
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print ('I owe the grocer $%.2f' % grocery_bill)
