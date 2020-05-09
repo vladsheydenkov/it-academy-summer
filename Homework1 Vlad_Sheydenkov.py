6120a89b4decaba66eddc1671226de0fe047939c
vladsheydenkov <vladsheydenkov@gmail.com>
2020-05-09 13:13:26 +0300

    error correction Homework1: Vlad Sheydenkov

--- Homework1 Vlad_Sheydenkov.py
+++ Homework1 Vlad_Sheydenkov.py
@@ -61,7 +61,7 @@ grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
 print('I owe the grocer $%.2f' % grocery_bill)
 
-'''8 This program adds up integers that have been passed 
+'''8 This program adds up integers that have been passed
 
 as arguments in the command line
 '''
