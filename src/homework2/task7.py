aafda52469d7eadda04b1b2bfe8cb8734c7cd0d8
vlad sheydenkov <vladsheydenkov@gmail.com>
2020-07-09 21:07:10 +0300

    fix mistakes

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -60,25 +60,26 @@ b = filter(lambda a: not a % 2, m)
 print(sum(b))
 
 
-"""You are given an array 
-(which will have a length of at least 3, but could be very large) 
-containing integers. 
-The array is either entirely comprised of odd integers 
-or entirely comprised of even integers except for a single integer N. 
-Write a method that takes the array as an argument 
+"""You are given an array
+(which will have a length of at least 3, but could be very large)
+containing integers.
+The array is either entirely comprised of odd integers
+or entirely comprised of even integers except for a single integer N.
+Write a method that takes the array as an argument
 and returns this "outlier" N.
 """
+
+
 def find_outlier(integers):
     listEven = []
     listOdd = []
     for num in integers:
         if num % 2 == 0:
-            listEven.append(n)
+            listEven.append(num)
         else:
-            listOdd.append(n)
+            listOdd.append(num)
 
     if len(listEven) == 1:
         return listEven[0]
     else:
         return listOdd[0]
-
