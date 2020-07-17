9fcf5d99075147934ee77312abf38f019f6b5625
vlad sheydenkov <vladsheydenkov@gmail.com>
2020-07-17 23:22:53 +0300

    one line solution for task1

--- src/homework2/task1.py
+++ src/homework2/task1.py
@@ -14,17 +14,14 @@ def total_sum(m, n, s):
         'x rubles y kopecks'
     """
     # write your code here
-    m = m * s
-    n = n * s
-    if n > 100:
-        k = n // 100
-        n = n % 100
-        m = m + k
-    sum = str(m) + " rubles " + str(n) + " kopecks"
-    return sum  # write return value here
+    return str(m * s + (n * s // 100)) + " rubles " + str(n * s % 100) + " kopecks"
 
 
+
+
+"""
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
     m, n, s = '', '', ''
     print(total_sum(m, n, s))
+"""
